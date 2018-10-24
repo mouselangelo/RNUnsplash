fffd5e76960c82063e1f48a95edd247af368a980
Chetan <chetan.mouselangelo@gmail.com>
2018-10-24 12:25:00 +0200

    Hello

---
+++ app/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class UnsplashApp extends Component {
+
+    render() {
+        return (
+            <View style={{paddingTop: 32, padding: 20}}>
+                <Text>Hello</Text>
+            </View>
+        );
+    }
+
+}
+
+export default UnsplashApp;
